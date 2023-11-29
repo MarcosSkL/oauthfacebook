da6720cae890a3fd53ee0501ba0acc66c77f0573
Marcos <marcos.marsk@gmail.com>
2023-11-29 17:26:30 -0300

    pequenas alterações

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -4,12 +4,11 @@ const index = () => {
    return (
       <>
          <section className="h-screen container">
-            <div className="h-full">
-               <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-                  <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-                     <h1>User Home</h1>
-                  </div>
-               </div>
+            <div className='h-96 w-96'>
+
+            </div>
+            <div>
+               <h1>User Home</h1>
             </div>
          </section>
       </>
