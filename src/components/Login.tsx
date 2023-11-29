cbc27ffba700a7d3d07ed2070c1d4b4f8d1072bb
Marcos <marcos.marsk@gmail.com>
2023-11-29 17:07:18 -0300

    Mudança no botao do Facebook

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebook } from "react-icons/fa";
 
 const Login = () => {
    return (
@@ -25,18 +26,9 @@ const Login = () => {
                            <p className="mb-0 mr-4 text-lg">Entrar com</p>
 
 
-                           <button
-                              type="button"
-                              className="mx-1 h-9 w-9 rounded-full bg-primary uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-
-                              <svg
-                                 xmlns="http://www.w3.org/2000/svg"
-                                 className="mx-auto h-3.5 w-3.5"
-                                 fill="blue"
-                                 viewBox="0 0 24 24">
-                                 <path
-                                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-                              </svg>
+                           <button type="button" className='flex gap-1 text-lg items-center'>
+                              <span className='text-blue-500 font-bold'>facebook</span>
+                              <FaFacebook className="text-blue-500 text-xl" />
                            </button>
 
                         </div>
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -3,12 +3,9 @@ import React from 'react'
 const index = () => {
    return (
       <>
-         <section className="h-screen">
+         <section className="h-screen container">
             <div className="h-full">
-
                <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-
-
                   <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
                      <h1>User Home</h1>
                   </div>
