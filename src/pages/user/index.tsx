b1f375aad72778224cf23b4279ec3b6fe375585f
Marcos <marcos.marsk@gmail.com>
2023-11-30 15:59:25 -0300

    Pequenas Alterações no Design responsivo

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { getAuth, onAuthStateChanged, signOut, browserLocalPersistence, setPersistence } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import firebase from '../../services/firebase';
 import withAuth from '@/components/Hoc';
@@ -12,7 +12,7 @@ const Index = () => {
    const [userData, setUserData] = useState<any>(null);
 
    useEffect(() => {
-     
+
       const unsubscribe = onAuthStateChanged(auth, async (user) => {
          if (user) {
             const userId = user.uid;
@@ -40,7 +40,6 @@ const Index = () => {
    const handleLogout = async () => {
       const auth = getAuth();
       try {
-         await setPersistence(auth, browserLocalPersistence);
          await signOut(auth)
             .then(() => {
                console.log('Logout realizado com sucesso');
@@ -57,23 +56,26 @@ const Index = () => {
 
    return (
       <>
-         <section className="h-screen bg-white">
-            <div className="h-60 w-screen bg-[url('../../public/assets/polygonal19.jpg')] bg-cover" />
-            <div className='h-36 w-36 bg-gray-400 rounded-full ms-20 -mt-14 flex justify-center items-center'>
-               <img src={userData.photoURL} alt='foto perfil' className='rounded-full h-36 w-36' />
-            </div>
-            <div className='ms-64 -mt-20'>
-               <p className='font-semibold font-serif text-lg'>{userData.displayName}</p>
-               <p className='font-mono'>{userData.email}</p>
-            </div>
-            <div className='flex justify-end me-10 -mt-10 '>
-               <button
-                  type="button"
-                  onClick={handleLogout}
-                  className="inline-block rounded bg-red-500 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-sm transition duration-150 ease-in-out hover:shadow-xl"
-               >
-                  Sair
-               </button>
+         <section className='max-w-[1000px]'>
+            <div className="mx-auto w-screen h-full flex flex-col justify-center bg-white">
+               <div className="sm:h-60 h-40 sm:w-screen w-screen bg-[url('../../public/assets/polygonal19.jpg')] bg-cover">
+               </div>
+               <div className='h-36 w-36 bg-gray-400 rounded-full ms-20 -mt-14 flex justify-center items-center'>
+                  <img src={userData.photoURL} alt='foto perfil' className='rounded-full h-36 w-36' />
+               </div>
+               <div className='md:ms-60 md:-mt-20'>
+                  <p className='font-semibold font-serif text-lg'>{userData.displayName}</p>
+                  <p className='font-mono'>{userData.email}</p>
+               </div>
+               <div className='flex justify-end md:me-10 md:-mt-10 -mt-10 md:h-full '>
+                  <button
+                     type="button"
+                     onClick={handleLogout}
+                     className="inline-block rounded bg-red-500 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-sm transition duration-150 ease-in-out hover:shadow-xl"
+                  >
+                     Sair
+                  </button>
+               </div>
             </div>
          </section>
       </>
